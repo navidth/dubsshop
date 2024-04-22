6e35a82f426a4c78b023389d9011d7fca4c81351
Navidth <navidtaheri32@gmail.com>
2024-04-22 23:07:02 +0330

    loading

--- src/components/loading/LoadingSkeleton.tsx
+++ src/components/loading/LoadingSkeleton.tsx
@@ -1,44 +1,47 @@
-import React from 'react'
+import React from "react";
 import "../../styles/skeleton.css";
-import Moshavere from '../Moshavere';
-import ResualtCat from '../ResualtCat';
-import PriceRange from '../PriceBox';
+import Moshavere from "../Moshavere";
+import ResualtCat from "../ResualtCat";
+import PriceRange from "../PriceBox";
 
 function LoadingSkeleton() {
   return (
-    <div className="mt-5 d-flex justify-content-center container-lg">
-    <section className="article me-3 w3-animate-right">
-    <Moshavere />
-    <PriceRange />
-  </section>
-      <section className="card-prudoct me-sm-5 me-0 rounded mb-5 ">
-      <div className="container-sm">
-        <div className="d-flex align-items-baseline">
-        <p className="skeleton-text skeleton ms-2"></p>
-        <p className="skeleton-text skeleton"></p>
+    <div className="mt-5 d-flex mx-5">
+      <section className="article me-3">
+        <Moshavere />
+        <PriceRange />
+      </section>
+      <section
+        className="card-prudoct me-sm-5 me-0 rounded mb-5 "
+        style={{ flex: "1" }}
+      >
+        <div className="container-sm">
+          <div className="d-flex align-items-baseline">
+            <p className="skeleton-text skeleton ms-2"></p>
+            <p className="skeleton-text skeleton"></p>
+          </div>
+          <hr className="m-0" />
+          <p className="skeleton-text skeleton mt-2"></p>
         </div>
-        <hr className="m-0" />
-        <p className="skeleton-text skeleton mt-2"></p>
-      </div>
-      <section className="mx-1 g-3 align-items-center  sectionAllProduct">
-        {[...new Array(6)].map((product, index) => (
-          <div className="collectionProduct  mt-4" key={index}>
-            <div className="cardProductLink">
-              <div className="skeleton-card   rounded-3 bg-white">
-                <div className="skeleton-card-img skeleton"></div>
+        <section className="mx-1 g-3 align-items-center  sectionAllProduct">
+          {[...new Array(6)].map((product, index) => (
+            <div className="collectionsProduct  mt-4" key={product}>
+              <div className="cardProductLink">
+                <div className="skeleton-card   rounded-3 bg-white">
+                  <div className="skeleton-card-img skeleton"></div>
 
-                <div className="">
-                  <p className="skeleton-text skeleton"></p>
-                  <p className="skeleton-text skeleton"></p>
+                  <div className="">
+                    <p className="skeleton-text skeleton"></p>
+                    <p className="skeleton-text skeleton"></p>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </section>
       </section>
-    </section>
     </div>
-  )
+  );
 }
 
-export default LoadingSkeleton
+export default LoadingSkeleton;
